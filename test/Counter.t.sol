3cbd6068f2ce66d6f6300347022b39c8e50570d2
Sigit Haryoyudanto <hello@sigit.co.uk>
2023-08-08 02:11:43 +0000

    commit

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console2} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import "forge-std/Test.sol";
+import "../src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
